1a4d62aa313c2fdb943bbffcfcba4eacbbed3aec
emilianots <emiliano@alu.ufc.br>
2019-05-04 19:50:15 -0300

    Criação do layout do carousel sem funcionalidade

--- devise/src/app/app.module.ts
+++ devise/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeBodyComponent } from './home-body/home-body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeBodyComponent
   ],
   imports: [
     BrowserModule,
---
+++ devise/src/app/home-body/home-body.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-body',
+  templateUrl: './home-body.component.html',
+  styleUrls: ['./home-body.component.css']
+})
+export class HomeBodyComponent {
+
+  
+  constructor() { }
+
+}/* 
+const track = document.querySelector('.carousel-track');
+const slides = Array.from(track.children);
+const nextBtn = document.querySelector('.c-b-right');
+const prevBtn = document.querySelector('.c-b-left');
+const dotsNav = document.querySelector('.carousel-nav');
+const dots = Array.from(dotsNav.children);
+
+const slideSize = slides[0].getBoundingClientRect().width;
+console.log(slideSize) */
