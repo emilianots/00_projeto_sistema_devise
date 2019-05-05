e6fc37da94dc5a9a45e7053ed3b80d04e968584e
emilianots <emiliano@alu.ufc.br>
2019-05-04 21:39:19 -0300

    Criados os novos componentes do corpo do site: card de imagem

--- devise/src/app/app.module.ts
+++ devise/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeBodyComponent } from './home-body/home-body.component';
+import { CardImgPanelComponent } from './card-img-panel/card-img-panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeBodyComponent
+    HomeBodyComponent,
+    CardImgPanelComponent
   ],
   imports: [
     BrowserModule,
---
+++ devise/src/app/card-img-panel/card-img-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-img-panel',
+  templateUrl: './card-img-panel.component.html',
+  styleUrls: ['./card-img-panel.component.css']
+})
+export class CardImgPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
